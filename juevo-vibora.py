c2ff7ed019646a714626d886c7a76b9dcd6f524a
A00833604 <A00833604@tec.mx>
2022-03-23 10:58:37 -0600

    Cambio fruta

--- juevo-vibora.py
+++ juevo-vibora.py
@@ -41,6 +41,14 @@ def move():
         print('Snake:', len(snake))
         food.x = randrange(-15, 15) * 10
         food.y = randrange(-15, 15) * 10
+
+        if (-200 < food.x < 190 and -200 < food.y < 190): #Checa si la fruta se encuentra dentro de la pantalla
+            food.x = randrange(-15, 15) * 10
+            food.y = randrange(-15, 15) * 10
+            
+        else: #Si la fruta se encuentra fuera de la pantalla
+            food.x = 0 #Fruta aparece en x 0
+            food.y = 0 #Fruta aparece en y 0
     else:
         snake.pop(0)
 
